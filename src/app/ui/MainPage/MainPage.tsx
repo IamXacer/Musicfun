96391add993b7a5d0391b5c0a0e3ea92a7ca79f5
IamXacer <phanton7487@gmail.com>
2025-09-19 20:32:17 +0200

    Update configuration and source code for GitHub Pages deployment

--- src/app/ui/MainPage/MainPage.tsx
+++ src/app/ui/MainPage/MainPage.tsx
@@ -1,7 +1,7 @@
 export const MainPage = () => {
     return (
         <div>
-            <h1>Main page!DEPLOY!!</h1>
+            <h1>Main page!DEPLOY!2!</h1>
         </div>
     )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,5 @@ export default defineConfig({
       '@/': `${path.resolve(__dirname, 'src')}/`,
     },
   },
+  base: '/Musicfun/',  // Указание пути для GitHub Pages
 })
