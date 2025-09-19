5a49fbdbfbc4abc8bc68d995b669dc54308015f3
IamXacer <phanton7487@gmail.com>
2025-09-19 22:47:19 +0200

    Resolve merge conflicts

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,5 @@ export default defineConfig({
       '@/': `${path.resolve(__dirname, 'src')}/`,
     },
   },
-  base: '/Musicfun/',  // Указание пути для GitHub Pages
-
+  base: '/Musicfun/',  // Замените на название вашего репозитория
 })
