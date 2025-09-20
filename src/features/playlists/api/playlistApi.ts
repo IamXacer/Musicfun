9df09c976632d2e16efaa569d7980c2272bc15d5
IamXacer <phanton7487@gmail.com>
2025-09-20 23:01:49 +0200

    Resolve merge conflicts

--- src/features/playlists/api/playlistApi.ts
+++ src/features/playlists/api/playlistApi.ts
@@ -1,18 +1,19 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
+import type { PlaylistsResponse} from "@/features/playlists/api/playlistsApi.types.ts";
 
-export const playlistApi = createApi ({
-    reducerPath:'playlistApi',
+export const playlistsApi = createApi({
+    reducerPath: 'playlistsApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'https://musicfun.it-incubator.app/api/1.0/',
-        headers:{'API-KEY':'XXX'}
+        baseUrl: import.meta.env.VITE_BASE_URL,
+        headers: {
+            'API-KEY': import.meta.env.VITE_API_KEY,
+        },
     }),
-    endpoints: (build) => {
-        return ({
-            fetchPlaylist: build.query<any, any>({
-                query: (name) => `pokemon/${name}`,
-            }),
-        });
-    },
-    })
+    endpoints: (build) => ({
+        fetchPlaylists: build.query<PlaylistsResponse, void>({
+            query: () => 'playlists',
+        }),
+    }),
+})
 
-
+export const { useFetchPlaylistsQuery } = playlistsApi
--- src/features/playlists/ui/PlaylistsPage.tsx
+++ src/features/playlists/ui/PlaylistsPage.tsx
@@ -1,4 +1,9 @@
+import {useFetchPlaylistsQuery} from "@/features/playlists/api/playlistApi.ts";
+
 export const PlaylistsPage = () => {
+
+   const res = useFetchPlaylistsQuery()
+    console.log(res)
     return (
         <div>
             <h1>Playlists page</h1>
